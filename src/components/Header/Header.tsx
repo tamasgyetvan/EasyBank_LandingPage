da2e2dd7ee897255616818834f44131619b2ef97
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2024-07-06 12:35:05 +0200

    Add mobile layout to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,20 @@
 import { NavBar } from "../NavBar/NavBar";
 import { RequestButton } from "../RequestButton/RequestButton";
-
+import { useState } from "react";
 export function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsOpen(!isOpen);
+  }
   return (
     <header>
       <img src="public/logo.svg" alt="EveryBank Logo" />
-      <NavBar />
+      <NavBar toggle={toggleMenu} isOpen={isOpen} />
       <RequestButton />
+      <button className="mobileMenuButton" onClick={toggleMenu}>
+        <img src="/icon-hamburger.svg" alt="" />
+      </button>
     </header>
   );
 }
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,6 +1,11 @@
-export function NavBar() {
+type NavBarProps = {
+  toggle: () => void;
+  isOpen: boolean;
+};
+
+export function NavBar({ isOpen, toggle }: NavBarProps) {
   return (
-    <nav>
+    <nav className={isOpen ? "active" : "inactive"}>
       <ul>
         <li>
           <a href="#">Home</a>
@@ -18,6 +23,9 @@ export function NavBar() {
           <a href="#">Careers</a>
         </li>
       </ul>
+      <button className="closeMenu" onClick={toggle}>
+        <img src="/icon-close.svg" alt="" />
+      </button>
     </nav>
   );
 }
